54e167bf304d42e21e963d3401f3231f774cc076
bruce <brucec@hotmail.com>
2021-03-22 16:56:35 -0700

    setup for loop for num clauses

--- bruce.sh
+++ bruce.sh
@@ -3,3 +3,12 @@
 #comment
 
 echo "Hello World"
+
+declare -r CLAUSE_LENGTH=3
+declare -r NUM_VARIABLES=20
+
+for num_clauses in 20 40 60 80 100 120 140 160 180 200
+do
+    echo $num_clauses
+done
+
