8f30add51e033ba40749488513a8e8082299b8f0
bruce <brucec@hotmail.com>
2021-03-22 17:08:01 -0700

    got piping working to input makewff to output

--- bruce.sh
+++ bruce.sh
@@ -10,5 +10,11 @@ declare -r NUM_VARIABLES=20
 for num_clauses in 20 40 60 80 100 120 140 160 180 200
 do
     echo $num_clauses
+    #generated_problem=$(./makewff -cnf $CLAUSE_LENGTH $NUM_VARIABLES $num_clauses)
+    
+    output=$(./makewff -cnf $CLAUSE_LENGTH $NUM_VARIABLES $num_clauses|./walksat)
+
+    echo $output
+    
 done
 
